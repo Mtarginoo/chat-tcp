a325cd73240693ab9bc4e96388a49aafc4758460
saulpdr <42222972+saulpdr@users.noreply.github.com>
2019-11-06 23:14:00 -0300

    mensagens OK
    mensagens apenas na tela do servidor! falta mostrar as mensagens nas telas dos clientes

--- clienteTCP.py3.py
+++ clienteTCP.py3.py
@@ -1,14 +1,60 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
+# DEPARTAMENTO DE ENGENHARIA DE COMPUTACAO E AUTOMACAO
+# DISCIPLINA REDES DE COMPUTADORES (DCA0113)
+# AUTOR: PROF. CARLOS M D VIEGAS (viegas 'at' dca.ufrn.br)
+#
+# SCRIPT: Cliente de sockets TCP modificado para enviar texto minusculo ao servidor e aguardar resposta em maiuscula
+#
+
 # importacao das bibliotecas
 from socket import *
-
+from threading import Thread
+    
 # definicao das variaveis
 serverName = 'localhost' # ip do servidor
 serverPort = 65000 # porta a se conectar
 clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
 
-apelido = input('Digite o seu apelido: ')
+def inputMensagem():
+    msg = ''
+    while msg != 'sair()':
+        msg = input()
+        clientSocket.send(msg.encode('utf-8'))
+        
+def recvMensagem():
+    '''try:
+        recvmsg = clientSocket.recv(1024)
+        recvmsg = recvmsg.decode('utf-8')
+    except:'''
+    recvmsg = ''
+    while recvmsg != 'sair()':
+        if recvmsg != '':
+            print(recvmsg)
+        try:
+            recvmsg = clientSocket.recv(1024)
+            recvmsg = recvmsg.decode('utf-8')
+        except:
+            recvmsg = ''
+
+pedido = clientSocket.recv(1024)
+pedido = pedido.decode('utf-8')
+print(pedido)
+apelido = input()
 clientSocket.send(apelido.encode('utf-8')) # envia o texto para o servidor
+
+t1 = Thread(target=inputMensagem, args=())
+t1.start()
+
+t2 = Thread(target=recvMensagem, args=())
+t2.start()
+
+while t1.isAlive():
+        continue
+
+while t2.isAlive():
+        continue
+
 # modifiedSentence = clientSocket.recv(1024) # recebe do servidor a resposta
 # print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
 clientSocket.close() # encerramento o socket do cliente
--- servidorTCP.py3.py
+++ servidorTCP.py3.py
@@ -1,6 +1,53 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE
+# DEPARTAMENTO DE ENGENHARIA DE COMPUTACAO E AUTOMACAO
+# DISCIPLINA REDES DE COMPUTADORES (DCA0113)
+# AUTOR: PROF. CARLOS M D VIEGAS (viegas 'at' dca.ufrn.br)
+#
+# SCRIPT: Servidor de sockets TCP modificado para receber texto minusculo do cliente enviar resposta em maiuscula
+#
+
 # importacao das bibliotecas
 from socket import * # sockets
-import threading
+from threading import Thread
+
+"""def inputMensagem():
+    msg = ''
+    while msg != 'sair()':
+        connectionSocket.send(msg.encode('utf-8'))"""
+        
+def recvMensagem(connectionSocket, addr, apelido):
+    '''try:
+        recvmsg = connectionSocket.recv(1024)
+        recvmsg = recvmsg.decode('utf-8')
+    except:'''
+    recvmsg = ''
+    while recvmsg != 'sair()':
+        if recvmsg != '':
+            print('%s diz: %s' % (apelido, recvmsg))
+        try:
+            recvmsg = connectionSocket.recv(1024)
+            recvmsg = recvmsg.decode('utf-8')
+        except:
+            recvmsg = ''
+    connectionSocket.send(recvmsg.encode('utf-8'))
+
+def threadConnection(connectionSocket, addr):
+    pedido = 'Digite o seu apelido: '
+    pedido = pedido.encode('utf-8')
+    connectionSocket.send(pedido)
+    apelido = connectionSocket.recv(1024) # recebe o nickname do cliente
+    apelido = apelido.decode('utf-8')
+    # capitalizedSentence = sentence.upper() # converte em letras maiusculas
+    print ('%s entrou!' % (apelido))
+    """t2 = Thread(target=inputMensagem, args=())
+    t2.start()"""
+    t3 = Thread(target=recvMensagem, args=[connectionSocket, addr, apelido])
+    t3.start()
+    # connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
+    while t3.isAlive():
+        continue
+    connectionSocket.close() # encerra o socket com o cliente 
+    print('%s saiu!' %(apelido))
 
 # definicao das variaveis
 serverName = '' # ip do servidor (em branco)
@@ -9,17 +56,11 @@ serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
 serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
 print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
-
-def threadConnection(connectionSocket, addr):
-    apelido = connectionSocket.recv(1024) # recebe o nickname do cliente / recv(1024) <- tamanho do buffer, deve ser potência de 2
-    apelido = apelido.decode('utf-8')
-    # capitalizedSentence = sentence.upper() # converte em letras maiusculas
-    print ('%s entrou!' % (apelido))
-    # connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
-    connectionSocket.close() # encerra o socket com o cliente 
     
 while 1:
+    tcont = 0
     connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-    t1 = threading.Thread(target=threadConnection, args=(connectionSocket, addr))
+    t1 = Thread(target=threadConnection, args=(connectionSocket, addr))
     t1.start()
  
+serverSocket.close() # encerra o socket do servidor
