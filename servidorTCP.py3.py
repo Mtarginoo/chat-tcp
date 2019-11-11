4d83cf7c781545088f28a3cc1104b9b83f6c3db5
Saul Pedro <saulpedroma@hotmail.com>
2019-11-11 19:03:54 -0300

    Add files via upload

--- servidorTCP.py3.py
+++ servidorTCP.py3.py
@@ -8,40 +8,78 @@
 
 # importacao das bibliotecas
 from socket import * # sockets
-from threading import Thread
+from threading import Thread # threads
 
-def inputMensagem():
-    msg = ''
-    while msg != 'sair()':
-        connectionSocket.send(msg.encode('utf-8'))
+clientesConectados = list(); # lista global contendo que será utilizada para armazenar todos os clientes conectados
+socketList = list() # lista global que será utilizada para armazenar o socket de todos os clientes conectados
         
 def recvMensagem(connectionSocket, addr, apelido):
-    recvmsg = ''
+    global socketList 
+    for i in socketList:
+        if (i != connectionSocket):
+            i.send(('%s entrou!' % (apelido)).encode('utf-8')) # o servidor envia a mensagem para todos os clientes conectados, menos para o que enviou a mensagem
+    recvmsg = ''     
     while recvmsg != 'sair()':
-        if recvmsg != '':
-            print('%s diz: %s' % (apelido, recvmsg))
         try:
-            recvmsg = connectionSocket.recv(1024)
-            recvmsg = recvmsg.decode('utf-8')
+            x = recvmsg.split('(')
+            comando = x[0]
         except:
+            continue
+        if comando == 'privado':
+            y = x[1].split(')')
+            destino = y[0]
+            recvmsg = ('%s enviou em privado: '% (apelido) + y[1])
+            cont = 0
+            for nome in clientesConectados:
+                if nome == destino:
+                    socketList[cont].send(recvmsg.encode('utf-8'))
+                    break
+                else:
+                    cont = cont + 1
             recvmsg = ''
+        else:
+            if recvmsg != '':
+                recvmsg = ('%s diz: %s' % (apelido, recvmsg))  # trocar para do while
+                print(recvmsg)
+                for i in socketList:
+                    if (i != connectionSocket):
+                        i.send(recvmsg.encode('utf-8'))
+            try:
+                recvmsg = connectionSocket.recv(1024)
+                recvmsg = recvmsg.decode('utf-8')
+            except:
+                recvmsg = ''
     connectionSocket.send(recvmsg.encode('utf-8'))
+    for i in socketList:
+        if (i != connectionSocket):
+            i.send(('%s saiu!' % (apelido)).encode('utf-8'))
 
 def threadConnection(connectionSocket, addr):
     pedido = 'Digite o seu apelido: '
     pedido = pedido.encode('utf-8')
+    
     connectionSocket.send(pedido)
     apelido = connectionSocket.recv(1024) # recebe o nickname do cliente
     apelido = apelido.decode('utf-8')
+    print('addr do %s: %s' % (apelido, addr))
     # capitalizedSentence = sentence.upper() # converte em letras maiusculas
     print ('%s entrou!' % (apelido))
-    """t2 = Thread(target=inputMensagem, args=())
-    t2.start()"""
+    global clientesConectados
+    global socketList
+    clientesConectados.append(apelido)
+    socketList.append(connectionSocket)
+    
+    """t5 = Thread(target=inputMensagem, args=[connectionSocket, addr, apelido])
+    t5.start()"""
     t3 = Thread(target=recvMensagem, args=[connectionSocket, addr, apelido])
     t3.start()
     # connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
     while t3.isAlive():
         continue
+    for i in clientesConectados:
+        if i == apelido:
+            clientesConectados.remove(apelido)
+            socketList.remove(connectionSocket)
     connectionSocket.close() # encerra o socket com o cliente 
     print('%s saiu!' %(apelido))
 
@@ -57,5 +95,7 @@ while 1:
     connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
     t1 = Thread(target=threadConnection, args=(connectionSocket, addr))
     t1.start()
- 
+    
+    
+
 serverSocket.close() # encerra o socket do servidor
