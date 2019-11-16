26506124e4dba41c94364c69ad5a6298e73cf2f7
Matheus Targino <mtarginoo@gmail.com>
2019-11-15 21:35:09 -0300

    Add files via upload

--- clientTCP.py
+++ clientTCP.py
@@ -1,16 +1,88 @@
-# SCRIPT: Cliente de sockets TCP modificado para enviar texto minusculo ao servidor e aguardar resposta em maiuscula
-
-# importacao das bibliotecas
 from socket import *
-
+from threading import Thread
+import sys
+    
 # definicao das variaveis
-serverName = 'localhost' # ip do servidor
-serverPort = 65000 # porta a se conectar
-clientSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
-clientSocket.connect((serverName, serverPort)) # conecta o socket ao servidor
+serverName = 'localhost'                                                                  # ip do servidor
+serverPort = 65000                                                                        # porta a se conectar
+clientSocket = socket(AF_INET,SOCK_STREAM)                                                # criacao do socket TCP
+clientSocket.connect((serverName, serverPort))                                            # conecta o socket ao servidor
 
-sentence = input('Digite o texto em letras minusculas: ')
-clientSocket.send(sentence.encode('utf-8')) # envia o texto para o servidor
-modifiedSentence = clientSocket.recv(1024) # recebe do servidor a resposta
-print ('O servidor (\'%s\', %d) respondeu com: %s' % (serverName, serverPort, modifiedSentence.decode('utf-8')))
-clientSocket.close() # encerramento o socket do cliente
+def inputMensagem():                                                       
+    msg = ''
+    while msg != 'sair()':
+        msg = input()    
+        newMsg = encodeProtocol(msg, apelido)
+        clientSocket.send(newMsg.encode('utf-8'))
+        clientSocket.send(''.encode('utf-8'))
+        
+def recvMensagem():                                                                           
+    recvmsg = ''                          
+    while recvmsg != 'sair()':
+        if recvmsg != '':
+            print(recvmsg)
+        try:
+            recvmsg = clientSocket.recv(1024)
+            recvmsg = recvmsg.decode('utf-8')
+        except:
+            recvmsg = ''
+    print("encerrando conexão com o servidor...")
+
+
+def encodeProtocol(mensagem, apelido):
+    aux = mensagem.split('(')
+    if(aux[0] == 'privado'): 
+        aux2 = aux[1]
+        aux = aux2.split(')') 
+        nicknamePrivado = aux[0]
+        aux3 = mensagem.split(')')
+        mensagem = aux3[1]
+        nicknameMensagem = nicknamePrivado +'/*'+ mensagem #concatenando o nickname do destinatário da mensagem com a mensagem enviada em modo privado
+        mensagem = nicknameMensagem
+        comando = 'privado'
+    elif(aux[0] == 'sair'):
+        comando = 'sair()'
+        mensagem = ''
+    elif(aux[0] == 'lista'):
+        comando = 'lista'
+        mensagem = ''    
+    else:
+        comando = ''
+    apelidoAux = apelido
+    comandoAux = comando
+    tamanhoMsg = len(apelidoAux.encode()) + len(comandoAux.encode()) + len(mensagem) + 6
+    tamanhoTanho = len(str(tamanhoMsg)) 
+    novaMensagem = (str(tamanhoMsg + tamanhoTanho) + '/0' + apelido + '/0' + comando + '/0' + mensagem)
+    return novaMensagem
+    
+    
+
+
+pedido = clientSocket.recv(1024)
+pedido = pedido.decode('utf-8')
+print(pedido)
+apelido = input()
+apelidoBytes = apelido.encode()
+while(len(apelidoBytes) > 16):
+    print("O apelido deve conter no máximo 16 caracteres")
+    print(pedido)
+    apelido = input()
+    apelidoBytes = apelido.encode()
+
+clientSocket.send(apelido.encode('utf-8'))                                                # envia o texto para o servidor
+
+t1 = Thread(target=inputMensagem, args=())                                                # instancia a thread de envio de mensagens
+t1.start()                                                                                # incia a thread de envio de mensagens
+
+t2 = Thread(target=recvMensagem, args=())                                                 # instancia a thread de recebimento de mensagens
+t2.start()                                                                                # inicia a thread de recebimento de mensagens
+
+while t1.isAlive():                                                                       # executa enquanto a thread de envio está ativa (msg != 'sair()')
+        continue
+
+while t2.isAlive():                                                                       # executa enquanto a thread de recebimento de mensagens está ativa (rcvmsg != 'sair())
+        continue
+
+clientSocket.send(('%s saiu!' % apelido).encode('utf-8'))                                 # envia para o servidor que o cliente saiu
+
+clientSocket.close()                                          
